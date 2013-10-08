f6c261620a7f0843546e7b2abb0e2e84e18cc630
Pavel Volokitin <pavelvolokitin@gmail.com>
2013-10-08 17:37:50 +0600

    initial commit

---
+++ src/Pv/MyBundle/Controller/BaseController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Pv\MyBundle\Controller;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class BaseController extends Controller
+{
+    /**
+     * @return DocumentManager
+     */
+    protected function getDM()
+    {
+        return $this->get('doctrine.odm.mongodb.document_manager');
+    }
+
+    protected function addFlashMessage($type, $message)
+    {
+        /** @var Session $session */
+        $session = $this->get('session');
+
+        $session->getFlashBag()->add($type, $message);
+    }
+
+    protected function trans($id, $parameters = array(), $domain = null, $locale = null)
+    {
+        /** @var TranslatorInterface $translator */
+        $translator = $this->get('translator');
+
+        return $translator->trans($id, $parameters, $domain, $locale);
+    }
+
+    protected function isGranted($attributes)
+    {
+        return $this->get('security.context')->isGranted($attributes, $object = null);
+    }
+
+    protected function secure($attributes)
+    {
+        if (!$this->isGranted($attributes)) {
+            throw new AccessDeniedException();
+        }
+    }
+
+    /**
+     * @return FormBuilder
+     */
+    protected function createNamedFormBuilder($name, $type = 'form', $data = null, $options = array())
+    {
+        return $this->get('form.factory')->createNamedBuilder($name, $type, $data, $options);
+    }
+}
---
+++ src/Pv/MyBundle/PvMyBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Pv\MyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PvMyBundle extends Bundle
+{
+}
