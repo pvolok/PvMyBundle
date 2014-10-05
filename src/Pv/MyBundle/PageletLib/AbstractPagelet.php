783cadc039cd08bc8fa3c73501760ec4f6b08526
Pavel Volokitin <pavelvolokitin@gmail.com>
2014-10-05 12:46:11 +0600

    Added helpers to AbstractPagelet.

--- src/Pv/MyBundle/PageletLib/AbstractPagelet.php
+++ src/Pv/MyBundle/PageletLib/AbstractPagelet.php
@@ -2,10 +2,28 @@
 
 namespace Pv\MyBundle\PageletLib;
 
+use Pv\MyBundle\Helper\CC;
+use Pv\MyBundle\Helper\HH;
 use Symfony\Component\DependencyInjection\ContainerAware;
 
-class AbstractPagelet extends ContainerAware
+abstract class AbstractPagelet extends ContainerAware
 {
+    /**
+     * @return HH
+     */
+    protected function hh()
+    {
+        return $this->container->get('pv.hh');
+    }
+
+    /**
+     * @return CC
+     */
+    protected function cc()
+    {
+        return $this->container->get('pv.cc');
+    }
+
     protected function render($view, array $parameters = array())
     {
         return $this->container->get('templating')->render($view, $parameters);
